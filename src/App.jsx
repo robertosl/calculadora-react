aca0bbf680ae5dad64657d675f6268b5b5e9ef1d
robertosl <roberto.s.lorenco@gmail.com>
2021-11-03 23:48:57 -0300

    Componentes main e button

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Calculator from "./main/Calculator";
+
+import "./App.css";
+
+const App = _ => (
+  <div className="App">
+      <h1>Calculadora</h1>
+      <Calculator />
+  </div>
+);
+
+export default App;
